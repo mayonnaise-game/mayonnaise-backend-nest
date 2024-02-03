f03393eee878b3de58e2c0d5362053bd4060821a
jaychang99 <jaychang99@gmail.com>
2024-02-03 10:05:11 +0900

    init: 시작포트 8080 으로 변경 및 v1 으로 전역 버저닝 prefix

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.setGlobalPrefix('v1');
+
+  await app.listen(8080);
 }
 bootstrap();
